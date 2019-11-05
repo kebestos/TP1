86301018175a96a416c62825318c5bc65e61e6f1
kebestos <jordan.brackenier@edu.esiee.fr>
2019-11-04 19:21:37 -0500

    multiple images

--- app/src/main/java/com/jordan/tp1/PictureView.java
+++ app/src/main/java/com/jordan/tp1/PictureView.java
@@ -14,8 +14,11 @@ import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.view.Display;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -23,82 +26,83 @@ import java.util.ArrayList;
 
 public class PictureView extends View {
 
-    //nombre max d'image sur une ligne
-    private static final int MAX_POINTERS = 7;
-    //private Pointer[] mPointers = new Pointer[MAX_POINTERS];
+    //number of line and column where affect the images
+    private static final int column = 5;
+    private static final int line = 5;
 
     private Paint mPaint;
 
     private ArrayList<String> listImages;
 
-    //dimension de l'écran
+    private ArrayList<Bitmap> listBitmap;
+
+    //dimension of the screen
     private int width;
     private int height;
 
-    /*class Pointer {
-        float x = 0;
-        float y = 0;
-        int index = -1;
-        int id = -1;
-    }
-*/
-    //init la view
+    //scale
+    private ScaleGestureDetector mScaleGestureDetector;
+    private float mScale = 1f;
+
+    //initialize the view
     public PictureView(Context context) {
         super(context);
-        /*for (int i = 0; i<MAX_POINTERS; i++) {
-            mPointers[i] = new Pointer();
-        }*/
-        listImages = getAllShownImagesPath((Activity) context);
+
         mPaint = new Paint();
         mPaint.setColor(Color.WHITE);
 
-        //recup les dimensions de l'écran
+        //get list of filePath of the images
+        listImages = getAllShownImagesPath((Activity) context);
+        //set a list of bitmap with the list of images, and compress
+        listBitmap = saveAndCompressShownImage();
+
+        //get the dimension of the screen
         Display display = ((Activity) context).getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
         width = size.x;
         height = size.y;
+
+        mScaleGestureDetector = new ScaleGestureDetector(context, new ScaleGesture());
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        Bitmap bitmap = null;
 
-        //varialble temporaire pour gérer l'affichage
+        //temporary variable to fix the screen
         int templeft=0;
-        int tempWidth = width/MAX_POINTERS;
-        int tempHeight = height/MAX_POINTERS;
+        int temptop=0;
+        int tempWidth = width/column;
+        int tempHeight = height/line;
 
 
-            //for(String fileP : listImages){
+        for(Bitmap bitmap : listBitmap){
 
-                File f = new File(listImages.get(0));
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inPreferredConfig = Bitmap.Config.ARGB_8888;
-                //inresample
+            BitmapDrawable bD = new BitmapDrawable(bitmap);
+            bD.setBounds(templeft,temptop,tempWidth,tempHeight);
+            bD.draw(canvas);
 
-                try {
-                    //creation du bitmap en fonction du filePath
-                    bitmap = BitmapFactory.decodeStream(new FileInputStream(f), null, options);
-                    BitmapDrawable bD = new BitmapDrawable(bitmap);
+            if(tempWidth == width){
+                tempHeight += height/ line;
+                temptop += height/ line;
+            }
 
-                    //setPosition et largeur hauteru des images
-                    //reste à placer plusieurs images en fonction des dimension de l'ecran à l'aide des variable temporaire
-                    bD.setBounds(100,100,200,200);
-
-                    bD.draw(canvas);
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-            //}
+            if(tempWidth != width){
+                templeft += width/ column;
+                tempWidth += width/ column;
+            }
+            else{
+                tempWidth = width/ column;
+                templeft = 0;
+            }
+        }
     }
 
-    //recup une liste contenant le filePath de chaque image
     private ArrayList<String> getAllShownImagesPath(Activity activity) {
         Uri uri;
         Cursor cursor;
-        int column_index_data, column_index_folder_name;
+        int column_index_data;
         ArrayList<String> listOfAllImages = new ArrayList<String>();
         String absolutePathOfImage = null;
         uri = android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
@@ -110,13 +114,65 @@ public class PictureView extends View {
                 null, null);
 
         column_index_data = cursor.getColumnIndexOrThrow(MediaStore.MediaColumns.DATA);
-        column_index_folder_name = cursor
-                .getColumnIndexOrThrow(MediaStore.Images.Media.BUCKET_DISPLAY_NAME);
+
         while (cursor.moveToNext()) {
             absolutePathOfImage = cursor.getString(column_index_data);
 
             listOfAllImages.add(absolutePathOfImage);
         }
         return listOfAllImages;
+    }
+
+    @SuppressLint("WrongThread")
+    private ArrayList<Bitmap> saveAndCompressShownImage(){
+        ArrayList<Bitmap> bitmaplist = new ArrayList<Bitmap>();
+
+        for(String fileP : listImages){
+            Bitmap bitmap = null;
+
+            File f = new File(fileP);
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+
+            try {
+                bitmap = BitmapFactory.decodeStream(new FileInputStream(f), null, options);
+
+                ByteArrayOutputStream boas = new ByteArrayOutputStream();
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 20, boas);
+                bitmaplist.add(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return bitmaplist;
+    }
+
+    public class ScaleGesture extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            mScale *= detector.getScaleFactor();
+
+            invalidate();
+            return true;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        mScaleGestureDetector.onTouchEvent(event);
+
+        switch (event.getAction() & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_POINTER_DOWN:
+            case MotionEvent.ACTION_MOVE:
+
+                invalidate();
+                break;
+            case MotionEvent.ACTION_CANCEL:
+
+                invalidate();
+                break;
+        }
+        return true;
     }
 }
